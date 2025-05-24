0f30595216785c260b7d68e17923a3c657c66cb1
Alistair <zceeawe@ucl.ac.uk>
2025-05-24 20:50:52 +0100

    added 'Add event' functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
 import os.path
+import zoneinfo
+from tzlocal import get_localzone_name
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -9,6 +11,32 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+
+def get_system_tz() -> str | None:
+    #Gets local timezone from system
+    try:
+        return get_localzone_name()
+    except Exception as e:
+        print(f"Error getting system timezone: {e}")
+
+#Store local timezone in global var
+TIMEZONE = get_system_tz()
+
+def convert_str_to_time(time_str: str) -> datetime.time | None:
+    #Converts string input in the format HH:MM into timezone.time object
+    try:
+        return datetime.datetime.strptime(time_str, "%H:%M").time()
+    except ValueError as e:
+        print(f"Invalid time format: {e}")
+
+def convert_str_to_date(date_str: str) -> datetime.date | None:
+    #Converts string input in the format dd-mm-YYYY to a datetime.date object
+    try:
+        return datetime.datetime.strptime(date_str, "%d-%m-%Y").date()
+    except ValueError as e:
+        print(f"Invalid date format: {e}")
+
 
 def authenticate():
     #Creates the json access and refresh tokens to authenticate user to the application
@@ -35,10 +63,12 @@ def authenticate():
 
 
 def get_events_on_day(creds, date_str: str):
+    #Returns a list of all events on a given day
     try:
         service = build("calendar", "v3", credentials=creds)
 
-        date = datetime.datetime.strptime(date_str, "%d-%m-%Y").date()
+        #Convert string date input to the correct datetime format
+        date = convert_str_to_date(date_str)
         start_datetime = datetime.datetime.combine(date, datetime.time.min).isoformat() + 'Z'
         end_datetime = datetime.datetime.combine(date, datetime.time.max).isoformat() + 'Z'
 
@@ -50,6 +80,7 @@ def get_events_on_day(creds, date_str: str):
             orderBy='startTime'
         ).execute()
 
+
         events = events_result.get('items', [])
 
         return events
@@ -58,11 +89,47 @@ def get_events_on_day(creds, date_str: str):
         print(f"An error occurred: {error}")
         quit()
 
-def main():
-    creds = authenticate()
 
-    events = get_events_on_day(creds, "21-05-2025")
+def add_event(creds, date_str: str, start_time_str: str, duration: int, summary: str)-> None:
+    #Adds an event to the Google calendar
 
+    #Convert date and time to the correct format for request body
+    date = convert_str_to_date(date_str)
+    start_time = convert_str_to_time(start_time_str)
+    start_dt = datetime.datetime.combine(date, start_time, tzinfo=zoneinfo.ZoneInfo(TIMEZONE))
+    end_dt = start_dt + datetime.timedelta(hours=duration)
+    start_formatted = start_dt.isoformat()
+    end_formatted = end_dt.isoformat()
+
+
+    #Create request body
+    event = {
+        'summary': summary,
+        'start': {
+            'dateTime': start_formatted,
+            'timeZone': TIMEZONE,
+        },
+        'end': {
+            'dateTime': end_formatted,
+            'timeZone': TIMEZONE,
+        },
+        'reminders': {
+            'useDefault': True,
+        }
+    }
+
+    #Send API call to insert event into the calendar
+    try:
+        service = build("calendar", "v3", credentials=creds)
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print(f"Event created: {event.get('htmlLink')}")
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        quit()
+
+def print_events(events):
+    #Prints list of events to the terminal (for debug purposes)
     if not events:
       print("No upcoming events found.")
       return
@@ -71,6 +138,13 @@ def main():
       start = event["start"].get("dateTime", event["start"].get("date"))
       print(start, event["summary"])
 
+def main():
+    creds = authenticate()
+
+    add_event(creds, "24-05-2025", "21:00", 2, "Test API")
+
+    events = get_events_on_day(creds, "24-05-2025")
+    print_events(events)
 
 if __name__ == "__main__":
   main()

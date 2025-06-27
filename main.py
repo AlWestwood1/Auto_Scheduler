3e3f0c0f4eb3c95238cc31828a9f238340f4e829
Alistair <zceeawe@ucl.ac.uk>
2025-06-27 17:06:51 +0100

    Added flexible event basic functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tzlocal import get_localzone_name
 import sqlite3
 import sys
 from typing import Tuple
+from abc import ABC
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -12,14 +13,17 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-class Event:
+class Event(ABC):
     def __init__(self, summary: str,
                  start_dt: datetime,
                  end_dt: datetime,):
 
         self.start_dt: datetime = start_dt
         self.end_dt: datetime = end_dt
+        self.is_flexible: bool = False
         self.duration: float = (end_dt - start_dt).total_seconds()
         self.summary: str = summary
         self.valid_start_dt: datetime = start_dt
@@ -28,6 +32,102 @@ class Event:
 
     def get_start_end_dt(self) -> Tuple[datetime, datetime]:
         return self.start_dt, self.end_dt
+
+    def add_to_calendar(self, creds) -> None:
+        # Adds an event to the Google calendar
+
+        # Convert date and time to the correct format for request body
+        start_formatted = self.start_dt.isoformat()
+        end_formatted = self.end_dt.isoformat()
+
+        # Create request body
+        event = {
+            'summary': self.summary,
+            'start': {
+                'dateTime': start_formatted,
+                'timeZone': TIMEZONE,
+            },
+            'end': {
+                'dateTime': end_formatted,
+                'timeZone': TIMEZONE,
+            },
+            'reminders': {
+                'useDefault': True,
+            }
+        }
+        # Send API call to insert event into the calendar
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            event = service.events().insert(calendarId='primary', body=event).execute()
+            print(f"Event created: {event.get('htmlLink')}")
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            sys.exit(1)
+
+    def is_duplicate(self, db_name: str) -> bool:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        # Check if event already exists in the DB
+        cursor.execute('''
+                       SELECT 1
+                       FROM events
+                       WHERE summary = ?
+                         AND event_start_dt = ?
+                         AND event_end_dt = ?
+                       LIMIT 1
+                       ''', (self.summary, self.start_dt.isoformat(), self.end_dt.isoformat()))
+
+        if cursor.fetchone():
+            print(f"Event with name '{self.summary}' already exists in database")
+            cursor.execute('''
+            UPDATE events SET last_updated = ?
+            WHERE summary = ?
+                AND event_start_dt = ?
+                AND event_end_dt = ?
+            ''', (datetime.now().isoformat(), self.summary, self.start_dt.isoformat(), self.end_dt.isoformat()))
+            conn.commit()
+            conn.close()
+            return True
+
+        conn.commit()
+        conn.close()
+
+        return False
+
+    def add_to_db(self, db_name: str) -> bool:
+        if self.is_duplicate(db_name):
+            return False
+
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        cursor.execute('''
+                       INSERT INTO events (summary, is_flexible, event_start_dt, event_end_dt, valid_start_dt,
+                                           valid_end_dt, timezone, last_updated)
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                       ''', (self.summary,
+                             int(self.is_flexible),
+                             self.start_dt.isoformat(),
+                             self.end_dt.isoformat(),
+                             self.valid_start_dt.isoformat(),
+                             self.valid_end_dt.isoformat(),
+                             TIMEZONE,
+                             datetime.now().isoformat()))
+
+        conn.commit()
+        conn.close()
+
+        print("Event added to database successfully")
+        return True
+
+    def submit_event(self, creds, db_name) -> None:
+        valid = self.add_to_db(db_name)
+        if valid:
+            self.add_to_calendar(creds)
+            print(f"Submitted event {self.summary}")
+
 
 class FixedEvent(Event):
     def __init__(self, summary: str,
@@ -47,7 +147,6 @@ class FixedEvent(Event):
                 f"start_dt: {self.start_dt}\n"
                 f"end_dt: {self.end_dt}\n"
                 f"duration: {self.duration}")
-
 
 class FlexibleEvent(Event):
     def __init__(self, summary: str,
@@ -76,8 +175,39 @@ class FlexibleEvent(Event):
         return self.valid_start_dt, self.valid_end_dt
 
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
+class EventBuilder(ABC):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def _generate_dts(date_str: str, start_time_str: str, end_time_str: str) -> Tuple[datetime, datetime]:
+        day = convert_str_to_date(date_str)
+        start_time = convert_str_to_time(start_time_str)
+        end_time = convert_str_to_time(end_time_str)
+        start_dt = datetime.combine(day, start_time, tzinfo=zoneinfo.ZoneInfo(TIMEZONE))
+        end_dt = datetime.combine(day, end_time, tzinfo=zoneinfo.ZoneInfo(TIMEZONE))
+
+        return start_dt, end_dt
+
+class FixedEventBuilder(EventBuilder):
+    def __init__(self):
+        super().__init__()
+
+    def create_fixed_event(self, date_str: str, start_time_str: str, end_time_str: str, summary: str) -> FixedEvent:
+        start_dt, end_dt = self._generate_dts(date_str, start_time_str, end_time_str)
+        return FixedEvent(summary, start_dt, end_dt)
+
+
+class FlexibleEventBuilder(EventBuilder):
+    def __init__(self):
+        super().__init__()
+
+    def create_flexible_event(self, date_str: str, valid_start_time_str: str, valid_end_time_str: str, duration: int, summary: str) -> FlexibleEvent:
+        valid_start_dt, valid_end_dt = self._generate_dts(date_str, valid_start_time_str, valid_end_time_str)
+        init_end_dt = valid_start_dt + timedelta(minutes=duration)
+
+        return FlexibleEvent(summary, valid_start_dt, init_end_dt, valid_start_dt, valid_end_dt)
+
 
 
 def get_system_tz() -> str:
@@ -90,60 +220,6 @@ def get_system_tz() -> str:
 
 #Store local timezone in global var
 TIMEZONE = get_system_tz()
-
-def create_table()-> None:
-    conn = sqlite3.connect(f"events.db")
-    print("Opened database successfully")
-
-    conn.execute('''
-    CREATE TABLE IF NOT EXISTS events (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    summary TEXT NOT NULL,
-    event_start_dt TEXT NOT NULL,
-    event_end_dt TEXT NOT NULL,
-    valid_start_dt TEXT,
-    valid_end_dt TEXT,
-    timezone TEXT NOT NULL
-    )
-    ''')
-
-    conn.commit()
-    conn.close()
-
-
-def add_event_to_db(event: Event) -> None:
-    conn = sqlite3.connect(f"events.db")
-    cursor = conn.cursor()
-
-    #Check if event already exists in the DB
-    cursor.execute('''
-        SELECT 1 FROM events
-        WHERE summary = ? AND event_start_dt = ? AND event_end_dt = ?
-        LIMIT 1
-    ''', (event.summary, event.start_dt.isoformat(), event.end_dt.isoformat()))
-
-    if cursor.fetchone():
-        print(f"Event with name '{event.summary}' already exists in database")
-        return
-
-    cursor.execute('''
-        INSERT INTO events (summary, event_start_dt, event_end_dt, valid_start_dt, valid_end_dt, timezone)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', (event.summary,
-          event.start_dt.isoformat(),
-          event.end_dt.isoformat(),
-          event.valid_start_dt.isoformat(),
-          event.valid_end_dt.isoformat(),
-          TIMEZONE))
-
-    conn.commit()
-    conn.close()
-
-    print("Event added to database successfully")
-
-
-
-
 
 def convert_str_to_time(time_str: str) -> time:
     #Converts string input in the format HH:MM into timezone.time object
@@ -160,6 +236,28 @@ def convert_str_to_date(date_str: str) -> date:
     except ValueError as e:
         print(f"Invalid date format: {e}")
         sys.exit(1)
+
+def create_table()-> None:
+    conn = sqlite3.connect(f"events.db")
+    print("Opened database successfully")
+
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    summary TEXT NOT NULL,
+    is_flexible INTEGER NOT NULL,
+    event_start_dt TEXT NOT NULL,
+    event_end_dt TEXT NOT NULL,
+    valid_start_dt TEXT,
+    valid_end_dt TEXT,
+    timezone TEXT NOT NULL,
+    last_updated TEXT NOT NULL
+    )
+    ''')
+
+    conn.commit()
+    conn.close()
+
 
 
 def authenticate():
@@ -230,39 +328,6 @@ def user_create_event(date_str: str, start_time_str: str, duration: int, is_flex
 
     return event
 
-def add_event_to_calendar(creds, event: Event)-> None:
-    #Adds an event to the Google calendar
-
-    #Convert date and time to the correct format for request body
-    start_formatted = event.start_dt.isoformat()
-    end_formatted = event.end_dt.isoformat()
-
-
-    #Create request body
-    event = {
-        'summary': event.summary,
-        'start': {
-            'dateTime': start_formatted,
-            'timeZone': TIMEZONE,
-        },
-        'end': {
-            'dateTime': end_formatted,
-            'timeZone': TIMEZONE,
-        },
-        'reminders': {
-            'useDefault': True,
-        }
-    }
-
-    #Send API call to insert event into the calendar
-    try:
-        service = build("calendar", "v3", credentials=creds)
-        event = service.events().insert(calendarId='primary', body=event).execute()
-        print(f"Event created: {event.get('htmlLink')}")
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        sys.exit(1)
 
 def print_events(events):
     #Prints list of events to the terminal (for debug purposes)
@@ -274,8 +339,8 @@ def print_events(events):
       start = event["start"].get("dateTime", event["start"].get("date"))
       print(start, event["summary"])
 
-def add_events_on_day_to_db(creds, date_str: str):
-    #Adds existing events from the google calendar on a given day to the DB
+def add_events_on_day_to_db(creds, db_name: str, date_str: str):
+    #Adds existing events from the Google calendar on a given day to the DB
     events = get_events_on_day(creds, date_str)
     for event in events:
         start_str = event["start"].get("dateTime", event["start"].get("date"))
@@ -284,21 +349,23 @@ def add_events_on_day_to_db(creds, date_str: str):
         start_dt = datetime.fromisoformat(start_str)
         end_dt = datetime.fromisoformat(end_str)
 
-        add_event_to_db(FixedEvent(event["summary"], start_dt, end_dt))
+        FixedEvent(event["summary"], start_dt, end_dt).add_to_db(db_name)
 
 
 
 def main():
     creds = authenticate()
-
-    new_event = user_create_event("21-06-2025", "16:00", 30, False, "Test API")
+    add_events_on_day_to_db(creds, "events.db", "27-06-2025")
+    eb = FlexibleEventBuilder()
+    new_event = eb.create_flexible_event("27-06-2025", "18:00", "23:00", 30, "Test API Flex")
     #print(new_event)
-    add_event_to_calendar(creds, new_event)
+    new_event.submit_event(creds, db_name="events.db")
 
-    add_events_on_day_to_db(creds, "21-06-2025")
+
 
 
 if __name__ == "__main__":
     main()
+    #create_table()
 
 
